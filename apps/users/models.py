dd5a4b83371c4f7217422d93a5ae989b9a9f22a7
Oleg Goncharov <oleg.gnch@gmail.com>
2020-09-03 00:41:35 +0300

    Создано приложение users

---
+++ apps/users/models.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+
+User = get_user_model()
+
+
+class Subscription(models.Model):
+    """
+    Модель, описывающая подписку одного пользователя (follower)
+    на другого (following).
+    """
+
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='followers',
+        verbose_name='Автор',
+    )
+    follower = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='subscriptions',
+        verbose_name='Подписчик'
+    )
+
+    class Meta:
+        unique_together = ('following', 'follower')
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+
+    def __str__(self):
+        return f'Subscription {self.follower} --> {self.following}'
